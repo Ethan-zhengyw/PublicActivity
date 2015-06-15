ff4b08c1fc058a0e9773055229678b473e92441a
ZSZ <zhengszh3@gmail.com>
2015-06-15 23:39:17 +0800

    del csrf setting

--- PublicActivity/settings.py
+++ PublicActivity/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.csrf.CsrfResponseMiddleware'
 )
 
 ROOT_URLCONF = 'PublicActivity.urls'
